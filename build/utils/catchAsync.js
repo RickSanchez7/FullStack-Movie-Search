fce6bb36e87363532f2bbcf5f75bfea80ffa6b42
RickSanchez7 <61505211+RickSanchez7@users.noreply.github.com>
2020-12-22 00:01:31 +0000

    change build folder

---
+++ build/utils/catchAsync.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = (function (fn) { return function (req, res, next) {
+    fn(req, res, next).catch(function (err) { return next(err); });
+}; });
